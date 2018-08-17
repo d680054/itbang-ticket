0e4003263af2cc24ae6622fb4e133325c9203b07
david.zheng <david.zheng@pactera.com.au>
2018-08-17 14:34:21 +1000

    bash 脚本更新,

--- bin/ticket.sh
+++ bin/ticket.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-if type -p java; then
-    _java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    _java="$JAVA_HOME/bin/java"
-else
-    echo "*****************Error: no java installed***************"
-    exit
-fi
+# Check if Java is present and the minimal version requierement
+MINIMAL_VERSION=1.8.0
 
-if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "********************found Java Version: $version******************"
-    if [[ "$version" < "1.8" ]]; then
-        echo need java version 1.8
-    fi
+_java=`type java | awk '{ print $ NF }'`
+CURRENT_VERSION=`"$_java" -version 2>&1 | awk -F'"' '/version/ {print $2}'`
+minimal_version=`echo $MINIMAL_VERSION | awk -F'.' '{ print $2 }'`
+current_version=`echo $CURRENT_VERSION | awk -F'.' '{ print $2 }'`
+if [ $current_version ]; then
+        if [ $current_version -lt $minimal_version ]; then
+                 echo "Error: Java version is too low to run ITBang-Ticket. Needs at least Java >= ${MINIMAL_VERSION}." 
+                 exit 1
+        fi
+    else
+         echo "Not able to find Java executable or version. Please check your Java installation."
+         exit 1
 fi
 
 pid=$(jps -v | grep "itbang" | awk '{print $1}')
